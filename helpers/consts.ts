9d2932b6fb05ad158e5e956cddbad4525a8d2c8a
Akash Gajjar <akashgajjar8@gmail.com>
2024-02-06 21:46:58 -0500

    change user and host

--- helpers/consts.ts
+++ helpers/consts.ts
@@ -1,9 +1,9 @@
 export const INTRO_TEXT = [
-  'Welcome to Aka.sh!',
+  'Welcome to akash.is!',
   'Type "help" to get started.',
   '',
 ];
 
-export const USER = 'skywalker';
+export const USER = 'skywalker212';
 
-export const HOST = 'fullmetal';
+export const HOST = 'akash.is';
